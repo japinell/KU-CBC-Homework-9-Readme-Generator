13e1e56bac735e2a4e8ba0b586f47f2cf9c4c0f4
JAPinell <japinell@yahoo.com>
2021-05-15 06:29:34 -0500

    Created functions to validate the inputs (alpha-numeric, alpha-only, and numeric-only)

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const inquirer = require("inquirer");
 const questions = [
   {
     type: "input",
+    name: "continue",
+    message:
+      "\n\n---------- README GENERATOR ----------\n\nThe following questions will be used to generate the blueprint for a professional Readme File.\nPress enter to continue...",
+  },
+  {
+    type: "input",
     name: "title",
     message:
       "\n\n---------- BASIC REQUIREMENTS ----------\n\nEnter the project title:",
     validate: (answer) => {
-      return validateInput(answer);
+      return validateAlphaNumericInput(answer);
     },
   },
   {
@@ -17,26 +23,61 @@ const questions = [
     name: "description",
     message: "Enter the project description:",
     validate: (answer) => {
-      return validateInput(answer);
+      return validateAlphaInput(answer);
     },
   },
   {
-    type: "list",
+    type: "confirm",
     name: "tableOfContent",
     message: "Do you want to include a table of content?",
-    choices: ["Y", "N"],
-    default: "Y",
+  },
+  {
+    type: "confirm",
+    name: "license",
+    message:
+      "\n\n---------- OPTIONAL REQUIREMENTS ----------\n\nDo you want to include a badge for the type of license?",
+  },
+  {
+    type: "confirm",
+    name: "features",
+    message: "Do you want to include a section for features?",
+  },
+  {
+    type: "confirm",
+    name: "contribute",
+    message: "Do you want to include a section for contributions?",
+  },
+  {
+    type: "confirm",
+    name: "tests",
+    message: "Do you want to include a section for tests?",
   },
 ];
 
-// Validate that the input is 2 or more words and at least 1 character each word
-function validateInput(input) {
+// Validate that the input is alpha-numeric with a lenght of 2 or more words, and at least 1 character each word
+function validateAlphaNumericInput(input) {
   let message =
     "Please enter two or more words with at least one letter or digit";
   if (input.length > 0) {
     return input.match(/^[a-zA-Z0-9]{1,}[\s]{1,}[a-zA-Z0-9]{1,}$/g)
       ? true
       : message;
+  } else return message;
+}
+
+// Validate that the input is alphabetical with a lenght of 2 or more words, and at least 1 character each word
+function validateAlphaInput(input) {
+  let message = "Please enter two or more words with letters only";
+  if (input.length > 0) {
+    return input.match(/^[a-zA-Z]{1,}[\s]{1,}[a-zA-Z]{1,}$/g) ? true : message;
+  } else return message;
+}
+
+// Validate that the input is numeric with a lenght of 2 or more words, and at least 1 character each word
+function validateNumericInput(input) {
+  let message = "Please enter two or more words with digits only";
+  if (input.length > 0) {
+    return input.match(/^[0-9]{1,}[\s]{1,}[0-9]{1,}$/g) ? true : message;
   } else return message;
 }
 
